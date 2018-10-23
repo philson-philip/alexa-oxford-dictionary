6046c7fe592a4753ba531edd8d6ceb9b83e243a3
Philson K Philip <philson.k.philip@gmail.com>
2018-10-23 13:02:46 +0530

    added

--- index.js
+++ index.js
@@ -36,18 +36,19 @@ alexaApp.intent("nameIntent", {
   ]
 },
 function(request, response) {
-  response.say("Success!");
+  var Dictionary = require("oxford-dictionary-api");
+  var app_id = "ed07b36c";
+  var app_key = "3fe0e2f2f85f13cbfaa906fbd2d16a98";
+  var dict = new Dictionary(app_id, app_key);
+  dict.find("hello",function(error,data){
+    if(error)
+      return console.log(error);
+    else
+    var definition = data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0];
+    return console.log(definition);
+    });
+  response.say(definition);
 }
 );
 app.listen(PORT, () => console.log("Listening on port " + PORT + "."));
-var Dictionary = require("oxford-dictionary-api");
-var app_id = "ed07b36c";
-var app_key = "3fe0e2f2f85f13cbfaa906fbd2d16a98";
-var dict = new Dictionary(app_id, app_key);
-dict.find("hello",function(error,data){
-  if(error)
-    return console.log(error);
-  else
-   var definition = data["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0];
-   return console.log(definition);
-  });
+
