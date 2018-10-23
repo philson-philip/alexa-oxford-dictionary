2f71bd213c2c13b6ac4a12b8fe59ccd65d209204
Philson K Philip <philson.k.philip@gmail.com>
2018-10-23 12:16:54 +0530

    added

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+node index.js
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 var Dictionary = require("oxford-dictionary-api");
 var app_id = "ed07b36c";
 var app_key = "3fe0e2f2f85f13cbfaa906fbd2d16a98";
 var dict = new Dictionary(app_id, app_key);
-dict.find("ace",function(error,data){
+dict.find("hello",function(error,data){
   if(error)
     return console.log(error);
   else
